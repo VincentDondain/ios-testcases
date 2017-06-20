9663f8028526c40ea8cd1482d08dec9a056b90d5
Vincent Dondain <vidondai@microsoft.com>
2017-06-20 11:09:50 -0400

    [ARPointCloudTest] Add ARKit test case

---
+++ ARPointCloudTest/ARPointCloudTest/ViewController.designer.cs
@@ -0,0 +1,25 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace ARPointCloudTest
+{
+	[Register("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		ARKit.ARSCNView SceneView { get; set; }
+
+		void ReleaseDesignerOutlets()
+		{
+			if (SceneView != null)
+			{
+				SceneView.Dispose();
+				SceneView = null;
+			}
+		}
+	}
+}
